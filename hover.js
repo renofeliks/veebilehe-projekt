817e4625a56779166552a1388dd79f941306060f
marcusut <144730253+marcusut@users.noreply.github.com>
2023-12-02 19:58:27 +0200

    javascriptin'

--- hover.js
+++ hover.js
@@ -1,14 +1,26 @@
+// This function runs when the page has loaded.
 window.onload = function() {
-function PlaySound(soundobj) {
-  var thissound = document.getElementById(soundobj);
-  thissound.play();
-}
 
-function StopSound(soundobj) {
-  var thissound = document.getElementById(soundobj);
-  thissound.pause();
-  thissound.currentTime = 0;
-}
+  window.PlaySound = function(soundobj) {
+    var thissound = document.getElementById(soundobj);
+    thissound.play();
 
-//https://stackoverflow.com/a/14926552
+    // Gradually increase the volume to 50% over 2 seconds.
+    var fadeInterval = setInterval(function() {
+      if (thissound.volume < 0.5) {
+        thissound.volume += 0.01;
+      } else {
+        // Stop the interval when the volume reaches 50%.
+        clearInterval(fadeInterval);
+      }
+    }, 20); // Increase volume by 1% every 20 milliseconds.
+  };
+
+  window.StopSound = function(soundobj) {
+    var thissound = document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+  };
 };
+
+//https://stackoverflow.com/a/14926552 ja Bing AI
