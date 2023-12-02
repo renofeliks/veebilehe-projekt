3aec557ce3d371b7ac6a73601244faeb4d11619a
marcusut <144730253+marcusut@users.noreply.github.com>
2023-12-02 18:18:17 +0200

    javascripting

--- pophover.js
+++ pophover.js
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<!-- This is your object. Replace it with your actual object. -->
-<div id="myObject" style="width: 100px; height: 100px; background: red;"></div>
-
-<!-- This is the audio file that will be played. -->
-<audio id="myAudio">
-  <source src="sound.mp3" type="audio/mpeg">
-</audio>
-
-<script>
 // Get the object and the audio elements.
 var object = document.getElementById("myObject");
 var audio = document.getElementById("myAudio");
@@ -25,7 +12,3 @@ object.addEventListener("mouseleave", function() {
   audio.pause();
   audio.currentTime = 0;
 });
-</script>
-
-</body>
-</html>
