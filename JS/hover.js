4f05e6167a3cbd5958a1faf214707d889a844ecc
marcusut <144730253+marcusut@users.noreply.github.com>
2023-12-04 17:44:46 +0200

    tegin hover muusika poole vaiksemaks igaks juhuks

--- JS/hover.js
+++ JS/hover.js
@@ -6,12 +6,12 @@ window.onload = function() {
     thissound.volume = 0; // Set initial volume to 0
     thissound.play();
 
-    // Gradually increase the volume to 100% over 4 seconds.
+    // Gradually increase the volume to 50% over 2 seconds.
     var fadeInterval = setInterval(function() {
-      if (thissound.volume <= 0.99) {
+      if (thissound.volume <= 0.5) {
         thissound.volume += 0.01;
       } else {
-        // Stop the interval when the volume reaches 99%.
+        // Stop the interval when the volume reaches 50%.
         clearInterval(fadeInterval);
       }
     }, 20); // Increase volume by 1% every 20 milliseconds.
