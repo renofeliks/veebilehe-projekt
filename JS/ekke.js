66d9a486c266d80c77863c4eae09695df532d368
Ekke <ekke.gross@gmail.com>
2023-12-05 21:44:23 +0200

    paar muudatust

--- JS/ekke.js
+++ JS/ekke.js
@@ -24,26 +24,26 @@ let track_list = [
   {
     name: "Beautiful (2023 Edit)",
     artist: "A. G. Cook",
-    image: "https://lh3.googleusercontent.com/pw/ADCreHc7n3ptSH_8sYLH4LgqpRzOqecENDmLh4qN1A1jCVfz5UcxRhP7-nPxhwKjald_LrJHGTRxgewiiuGTxsRhHaxhAX1jm3K0oI4_iZrGU3YM2wJoYJD4T0cFbDZaplQQdXg3L_IJ0m7FZNFMSg4vTdQC4DUkyZUKzhRTqAGnqxHGQT4mMyYbWRPFQvifwCbLA_Op8S7-XeVpBI3yyQSmzCqUlO0ffkZ2ciWfuQTtgs3PtuXk2YkyJx1AsBMuwqtA1ra_RbmSUjnra7YeQiv9xD5RSxqFNs7JgY0hN-h7iUXzJH40ekPDwBQh9GLyQ4apTlSUHPCoMAAWb4QP8Wultx7bLvYsj1Xak7wmc_9PLcMlbAfthV6xWkZw1iWzO-TZMvwGZyPDk3XxyNm6P3Eq11SrmodvTzbdUkALVZpEgMWnHsfpcOxKFEq6rMmJ_NUP-b3nouQsOFPpFEYyy6VpIJlUAaDMmiEsn18zM3VSMr8NCAJtwdcApntFKSrUj6WKToEYSKp0XT1Qk34am0Tt3exb57ZCV0ZAIobYukGA034JHDI68YxHb_Bu6aERhcFbmbCQ6UwBtBLS1_RlCj8XxiBOg9pgV1i3rvOtyY7WOIHmZApm5ISv70ifnJZ0G1_f7Rtrp0KZZX3hPygZMnXMyFRKMK5QWIKrIvhwp8SRAI-rqR4YJS3oUeU0WHV5BnhYtCX5-sPh9aVvWTUMgAPNpjKAYiBhX9kmT4dg17iUgm1LT1893GE5J2o6rHw37q5zawi59uJEFgvvyGUb8yTQvpEnV9NyvOQoWQgHizIjEBw-o3skbLQnFmjjJHa34_e7R3D4vyjeB603LPvgKLyr3dNlqAp2e533JSWkAOzlU4r9MMWXeE62SO0csUXP3IlitA=w700-h700-s-no-gm?authuser=0",
+    image: "https://f4.bcbits.com/img/a3211304548_16.jpg",
     path: "https://p.scdn.co/mp3-preview/b31a6562bc48dc158d5a734877d4fea02bee70d6"
   },
   {
     name: "BIPP",
     artist: "SOPHIE",
-    image: "https://lh3.googleusercontent.com/pw/ADCreHdYTl6GBnneiXIvybsrFeMjidqXZlg1kQUYCnFl5gc83Uz6nqa9FgX-kA5h7qIDa3O3bz09mOKoCWtC63pJXj5rTJeZlJuJalKlbUx0YkvXchLGCT3MutIDpkobMmjSCFYU7tdV6g7kJtzWpVY2UFXaEBllhLV7xZrZupYGsPzd4x75ICcYKYHOJVcGSnVJNcVMXtlzkDcMoU-oyO8WSfqUL_z2Z5t1zRCYaF6GPkJZ_g05HTyplJ8rGil5c2hXEnWTAcOHkjcADJFI263Cy7iJF5Y9KEvGqiZy_tqvkr4XLn5WX4bPToZ1Cfi8nuDF2EtSlxKSp9bqHxYdivDZ8-sAWqZCkrF_y4RT3wAiQpirVJxJNd1H-VIAf0uaP24QkNLbZGS_aXGQvKc4nE8YwuElqzVAA5yKx33rKJn_9FZEQ6Jmh5ZAMUD7jLW50k5HpoJeCrWcId0xwY1sGg3N-ObKb-bFSIV8NW4yS4CU83yUbw-SY4ClNINbU6udSCpZBzL3jev9ZI9pmqmfimXtD2T2Rrvo9KE8XBOxq1tlXyxlZW9M6OX0_-AAU6lvMDREaaVBpnusr9HTJGiTX_mM92FQzFv7j0gZQ0C4yg5AQdBAbiQ7SISlX777eVsB9CdVa8ip5gdeb_NonYhv1fIQeIhGXwFTl5L9VVKfCS1RkqdolFYLgW303NriDb1pfX5FAIbAMqztycbo4JggmhlWwpvrDnav7bDKROSyT1cyN-UCO7tw7_J9D30yR5sWsgeR9aoUk3iqwbKhiwCHztyNYkIUFQu4JurbKc3XOXJSBt2K1UqxUSyPlTCXg9WybtbHzrwH24nuNCBklL-gah5Cu2YmbssWbU1mbgDrBNOc-3ahLw_KpmBA7j_bgkZfUlr-vw=w316-h316-s-no-gm?authuser=0",
+    image: "https://f4.bcbits.com/img/a1966225446_16.jpg",
     path: "https://p.scdn.co/mp3-preview/cbf3d9b36086121c10f7cd9d448fa74f7ca157fc"
   },
   {
     name: "movinglikeazombie (Remix)",
     artist: "ericdoa, umru, SEBii, angelus, kmoe, Lewis Grant, savepoint, Tony Velour, emotegi, d0llywood1",
-    image: "https://lh3.googleusercontent.com/pw/ADCreHeJLnO8AQe-IO80Z2kjx4v5Lzi38VXhXtI_787fO_uVM7k4vPK5lH0WkAseihxjDeFTxlZ97pGHEzCOBdSLmsOW6wGSR5dQvjpnz0Dji-DzijcAzVeu2q7CIjOP__NxIYgcE0QYgLtZJhf8TnBbpuBoo8zJgR5mIgNtGDd_YD00pcvC1GkNHj2WzDPeVX-B8GUwNW8CxLu9yaUQvliEgqV9aBll4CeakTTmJd6ktQOexOSXJ2UzT5DUm11Cpa-AcxSiwlAcQGmVSSXNGjxfU4IbpwKUKwg6D7WGKnvhkdu9_kIXm2dU68NvMYswEiCCdFyRFnQcnfVx9lvhDkwnQ-0_MO16WVtdMz2ZuEugA5PgBgQcSKpfHK8OPnzPJ8MS_2LGtCKc-rTq4suj6lpCxuSJ3W1zbteWbc09suZgk2Mr6PMJUu6wH1ZxW3yAISBTi6CuGFuouXb--2Kmbj3keQtVvfLjlGpU2aZjVwPhcKcfn2FALsWIluQd35agSZaQVBUxEldyBcf72Yq3e4OubJgqgOkRzZrZro7BbjZI3j28nJH2OOxTEC3IuwCwqRjzJWgvvUMSJ5uDjb9_nh__afiMKF-wdo2KzSjJZwmiGqeIqRM4LeF2eFB_ImkTRxBxY6XcyObH2ISTOuQKizZdBXTNFrVK52I8N6Xm_4iK8WR_--ymjuIVM5vu0caIrX-by4UuG9yE1hGBXmMsWIp-9rGoChfKXAXlAdEXxjvW4iAtMF-y6hwASSfhWopAO1t_vs2Y5mdbDY_CactEqLe_ndSDhKlFQW75UbUNlxvhgNjVfcmBKu4ypXXrGb9Y67gcW7gnjUOyYXBoY49DdgjwXxliV53rIQxkkBR3yvBQmvTqdmP0D-LZtg6lzP7UEORJhA=w500-h500-s-no-gm?authuser=0",
-    path: "https://p.scdn.co/mp3-preview/9903cefddb9335cf6dd2e9401fb9a24d074607dc",
+    image: "https://f4.bcbits.com/img/a4163590065_16.jpg",
+    path: "https://p.scdn.co/mp3-preview/9903cefddb9335cf6dd2e9401fb9a24d074607dc"
   },
   {
     name: "X-RAY (Harlecore Re-Edit)",
     artist: "Danny L Harle, Tommy Cash",
-    image: "https://lh3.googleusercontent.com/pw/ADCreHctUArt8Em1vF8RpSF5Ru9UiBdMxVhVgbu-eAdk0dgkpko8gM9-fVeCFZUq-DbzvnBF1eF8CYcKcNeqbfc7wZuvEDQD_IANeCTtR_Zp4F2jyhdantEQWJE7YOdgVXAvLt8HdwO8vuZws_rIyA2sjZzQSuSJtUGARU4Ef4OsKuaFm58FOEoAvWM1UICQ425xNrUU1Z3HztmhXW7Sk316ObEQ6KS18FXbdA7ztleAMd2N-sjwGT3KktVvT965IfEEO31gtbjQzlnVQwFjTlJNOuNVFKJauagSiye5e0gHyITyleGeJN35fFBqJ9sd9LXiLsoaoZ473RglT0qboVn7O_rsKMdsneztaiNv1LoLNqeD6JHoziLug7ojavca9zN5hCKi5sOChr429h_xveb9qhEfCP0GpXZcUlKVwqnWmrvBL6CXYMKLSthObEm6lxyt27fywtKFVeU1I__eUztNlS8NMvkTKJQ67RBykZJ1Ev_hh8KwvPLUflAPi95uZZRPDG-_tAkOwmLi74WOJieAtgDUXRbSHDJXcVmRNGzYdPZgOz6BHw-FT-my4Kg8bb28wx3uurwmg7zAoqIhWW-1RtMhsz-ouadBBSALW2DXMgH_wBLJHZzxycmvVtAtayt_OcZW6OEEImK__ZcrO9RdQDTg4qJknVKt-ZaZKX-V2MMmLZaukNBvkr2zQ-P0PoBfdyOAbBj3IYJ8dEg4aL1Bw0g7bwNQuEcyw4LZPpUP1W6C3cwbtHXqne9Cv7X-LVmR5O5nOJmjwFonpheYQOKC2FBFkIEOPnLHoSdoOkBJo3TSeMWcaEgFMDCRkLv7J4C9pdViY72pRGgUZAw5CsoaZ6oMi4Jl8LCk5TPCMMbjyJRPT9z9UQ6TuxIOH1jR3RWdHw=w1200-h1200-s-no-gm?authuser=0",
-    path: "https://p.scdn.co/mp3-preview/adb60204d8a6201947db4bef990d24f28af1fbde",
+    image: "https://f4.bcbits.com/img/a4276142302_16.jpg",
+    path: "https://p.scdn.co/mp3-preview/adb60204d8a6201947db4bef990d24f28af1fbde"
   }, 
 ];
 
